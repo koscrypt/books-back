9658dda5af0a7855fce937499732200eb436df57
Davshn <davshn@gmail.com>
2023-08-19 12:06:44 -0500

    feat: www

--- src/middlewares/cors/index.ts
+++ src/middlewares/cors/index.ts
@@ -2,7 +2,7 @@ import type { CorsOptions } from 'cors'
 
 import Log from '../logger'
 
-const whitelist: string[] = ['http://localhost:4000', 'http://localhost:3000', 'https://leafy-loader-393200.rj.r.appspot.com', 'http://bibliomobil.com', 'https://biblio-pwa.vercel.app', 'https://bibliomobil.com']
+const whitelist: string[] = ['http://localhost:4000', 'http://localhost:3000', 'https://leafy-loader-393200.rj.r.appspot.com', 'https://www.bibliomobil.com', 'https://biblio-pwa.vercel.app', 'https://bibliomobil.com']
 
 const corsConfig = {
   origin: function (origin: string, response: (res: string | null, cont?: boolean) => void) {
