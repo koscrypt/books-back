1e4db8a5f072c6289bf7be98fdbecdb07b60d68f
Davshn <davshn@gmail.com>
2023-08-19 12:13:20 -0500

    feat: fix cors

--- src/middlewares/cors/index.ts
+++ src/middlewares/cors/index.ts
@@ -1,17 +1,12 @@
 import type { CorsOptions } from 'cors'
 
-import Log from '../logger'
+// import Log from '../logger'
 
-const whitelist: string[] = ['http://localhost:4000', 'http://localhost:3000', 'https://leafy-loader-393200.rj.r.appspot.com', 'https://www.bibliomobil.com', 'https://biblio-pwa.vercel.app', 'https://bibliomobil.com']
+// const whitelist: string[] = ['http://localhost:4000', 'http://localhost:3000', 'https://leafy-loader-393200.rj.r.appspot.com', 'https://www.bibliomobil.com', 'https://biblio-pwa.vercel.app', 'https://bibliomobil.com']
 
 const corsConfig = {
   origin: function (origin: string, response: (res: string | null, cont?: boolean) => void) {
-    if (whitelist.includes(origin) || origin === undefined) {
-      response(null, true)
-    } else {
-      Log.error('Origin not allowed ' + origin)
-      response('Origin not allowed')
-    }
+    response(null, true)
   }
 }
 
