09bd8a6d3457eb02397835eea6d0e5f67386f414
Steven Jaimes Galeano <steinu037@gmail.com>
2023-12-01 16:11:39 -0500

    deployer

--- src/middlewares/cors/index.ts
+++ src/middlewares/cors/index.ts
@@ -4,6 +4,8 @@ import type { CorsOptions } from 'cors'
 
 // const whitelist: string[] = ['http://localhost:4000', 'http://localhost:3000', 'https://leafy-loader-393200.rj.r.appspot.com', 'https://www.bibliomobil.com', 'https://biblio-pwa.vercel.app', 'https://bibliomobil.com']
 
+//
+
 const corsConfig = {
   origin: function (origin: string, response: (res: string | null, cont?: boolean) => void) {
     response(null, true)
